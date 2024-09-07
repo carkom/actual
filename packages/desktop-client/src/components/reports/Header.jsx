bf729ca17bd83434f776de70ddb96e3541699968
carkom <neilmccreery@yahoo.com>
2024-09-07 08:08:46 +0100

    header changes

--- packages/desktop-client/src/components/reports/Header.jsx
+++ packages/desktop-client/src/components/reports/Header.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import * as monthUtils from 'loot-core/src/shared/months';
 
 import { useResponsive } from '../../ResponsiveProvider';
+import { theme } from '../../style/theme';
 import { Button } from '../common/Button2';
 import { Select } from '../common/Select';
 import { View } from '../common/View';
 import { AppliedFilters } from '../filters/AppliedFilters';
 import { FilterButton } from '../filters/FiltersMenu';
 
+import { ModeButton } from './ModeButton';
 import {
   getFullRange,
   getLatestRange,
@@ -34,6 +37,9 @@ export function Header({
   const location = useLocation();
   const path = location.pathname;
   const { isNarrowWidth } = useResponsive();
+
+  const [isDateStatic, setIsDateStatic] = useState(false);
+  const [liveDate, setLiveDate] = useState(3);
 
   return (
     <View
@@ -61,89 +67,144 @@ export function Header({
               gap: 5,
             }}
           >
-            <Select
-              onChange={newValue =>
-                onChangeDates(
-                  ...validateStart(
-                    allMonths[allMonths.length - 1].name,
-                    newValue,
-                    end,
-                  ),
-                )
-              }
-              value={start}
-              defaultLabel={monthUtils.format(start, 'MMMM, yyyy')}
-              options={allMonths.map(({ name, pretty }) => [name, pretty])}
-            />
-            <View>to</View>
-            <Select
-              onChange={newValue =>
-                onChangeDates(
-                  ...validateEnd(
-                    allMonths[allMonths.length - 1].name,
-                    start,
-                    newValue,
-                  ),
-                )
-              }
-              value={end}
-              options={allMonths.map(({ name, pretty }) => [name, pretty])}
-              buttonStyle={{ marginRight: 10 }}
-            />
-            {filters && (
-              <FilterButton
-                compact={isNarrowWidth}
-                onApply={onApply}
-                type="accounts"
-              />
+            <ModeButton
+              selected={!isDateStatic}
+              onSelect={() => {
+                setIsDateStatic(false);
+              }}
+            >
+              Live
+            </ModeButton>
+            <ModeButton
+              selected={isDateStatic}
+              onSelect={() => {
+                setIsDateStatic(true);
+              }}
+            >
+              Static
+            </ModeButton>
+            {isDateStatic && (
+              <>
+                <Select
+                  onChange={newValue =>
+                    onChangeDates(
+                      ...validateStart(
+                        allMonths[allMonths.length - 1].name,
+                        newValue,
+                        end,
+                      ),
+                    )
+                  }
+                  value={start}
+                  defaultLabel={monthUtils.format(start, 'MMMM, yyyy')}
+                  options={allMonths.map(({ name, pretty }) => [name, pretty])}
+                />
+                <View>to</View>
+                <Select
+                  onChange={newValue =>
+                    onChangeDates(
+                      ...validateEnd(
+                        allMonths[allMonths.length - 1].name,
+                        start,
+                        newValue,
+                      ),
+                    )
+                  }
+                  value={end}
+                  options={allMonths.map(({ name, pretty }) => [name, pretty])}
+                  buttonStyle={{ marginRight: 10 }}
+                />
+              </>
             )}
           </View>
-
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 15,
-              flexWrap: 'wrap',
-            }}
-          >
-            {show1Month && (
+          {!isDateStatic && (
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 15,
+                flexWrap: 'wrap',
+              }}
+            >
+              {show1Month && (
+                <Button
+                  selected={liveDate === 1}
+                  onPress={() => {
+                    onChangeDates(...getLatestRange(1));
+                    setLiveDate(1);
+                  }}
+                  style={{
+                    ...(liveDate === 1 && {
+                      backgroundColor: theme.buttonPrimaryBackground,
+                    }),
+                  }}
+                >
+                  1 month
+                </Button>
+              )}
               <Button
-                variant="bare"
-                onPress={() => onChangeDates(...getLatestRange(1))}
+                onPress={() => {
+                  onChangeDates(...getLatestRange(2));
+                  setLiveDate(3);
+                }}
+                style={{
+                  ...(liveDate === 3 && {
+                    backgroundColor: theme.buttonPrimaryBackground,
+                  }),
+                }}
               >
-                1 month
+                3 months
               </Button>
-            )}
-            <Button
-              variant="bare"
-              onPress={() => onChangeDates(...getLatestRange(2))}
-            >
-              3 months
-            </Button>
-            <Button
-              variant="bare"
-              onPress={() => onChangeDates(...getLatestRange(5))}
-            >
-              6 months
-            </Button>
-            <Button
-              variant="bare"
-              onPress={() => onChangeDates(...getLatestRange(11))}
-            >
-              1 Year
-            </Button>
-            <Button
-              variant="bare"
-              onPress={() =>
-                onChangeDates(
-                  ...getFullRange(allMonths[allMonths.length - 1].name),
-                )
-              }
-            >
-              All Time
-            </Button>
-          </View>
+              <Button
+                onPress={() => {
+                  onChangeDates(...getLatestRange(5));
+                  setLiveDate(6);
+                }}
+                style={{
+                  ...(liveDate === 6 && {
+                    backgroundColor: theme.buttonPrimaryBackground,
+                  }),
+                }}
+              >
+                6 months
+              </Button>
+              <Button
+                onPress={() => {
+                  onChangeDates(...getLatestRange(11));
+                  setLiveDate(12);
+                }}
+                style={{
+                  ...(liveDate === 12 && {
+                    backgroundColor: theme.buttonPrimaryBackground,
+                  }),
+                }}
+              >
+                1 Year
+              </Button>
+              <Button
+                onPress={() => {
+                  onChangeDates(
+                    ...getFullRange(allMonths[allMonths.length - 1].name),
+                  );
+                  setLiveDate(0);
+                }}
+                style={{
+                  ...(liveDate === 0 && {
+                    backgroundColor: theme.buttonPrimaryBackground,
+                  }),
+                }}
+              >
+                All Time
+              </Button>
+            </View>
+          )}
+          {filters && (
+            <FilterButton
+              compact={isNarrowWidth}
+              onApply={onApply}
+              type="accounts"
+            />
+          )}
           {children || <View style={{ flex: 1 }} />}
         </View>
       )}
